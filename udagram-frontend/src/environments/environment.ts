ce6c048d014ba710badc078232ffb1ba696b950d
AAboElella <aaboelella@ejada.com>
2025-12-09 15:10:31 +0200

    change reversproxy FE URL to prod

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'ac221e34d27bd4c7ab447b31160dc248-200805809.us-east-1.elb.amazonaws.com/api/v0'
 };
 
 /*
